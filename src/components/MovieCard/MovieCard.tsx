d96f9ba878f0ee0592049e2f09fac3124b3018d4
Eduardo Parducci <eduardo.parducci@dfki.de>
2021-08-06 21:34:52 +0200

    feat: added movie thumbs.

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -3,13 +3,14 @@ import { Movie} from '../../models/movie'
 function MovieCard(props: {movie: Movie}) {
 
     function handleFavorite(movie: Movie) {
-        alert(`${movie.title} adicionado aos favoritos!`);        
+        alert(`${movie.title} adicionado aos favoritos!`);
     }
-
+    
     return (
-        <header>
-            <button onClick={() =>handleFavorite(props.movie)}>{props.movie.title}</button>
-        </header>
+        <div onClick={() =>handleFavorite(props.movie)}>
+            <img src={`${process.env.PUBLIC_URL}/assets/${props.movie.episode_id}.png`} alt="Movie thumb"/>
+            <h1>{props.movie.title}</h1>
+        </div>
     )
 }
 
