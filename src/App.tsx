3b19d1a8bc13723121cde338e7eb0314458b1968
Eduardo Parducci <eduardo.parducci@dfki.de>
2021-08-06 20:13:04 +0200

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ApiService from './services/api';
+import { Movie } from './models/movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    const [movies, setMovies] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        const api = new ApiService();
+        const getMovies = async () => {
+            try {
+                const response = await api.getMovies();
+                setMovies(response);
+            } catch(err) {
+                console.log(err);
+            }
+        }
+        getMovies();
+    }, []);
+
+    function handleFavorite(movie: Movie) {
+        alert(`${movie.title} adicionado aos favoritos!`);        
+    }
+    
+    return (
+        <ul>
+            {movies.map(movie => (
+                <li key={movie.episode_id}>
+                    {movie.title}
+                    <button onClick={() => handleFavorite(movie)}>Adicionar aos favoritos</button>
+                </li>))}
+        </ul>
+    );
 }
-
-export default App;
