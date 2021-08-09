ceb766bcdbb641001923b63df07da4c910c1daa3
Eduardo Parducci <eduardo.parducci@dfki.de>
2021-08-09 16:25:34 +0200

    feat: general jest test config.

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules"],
+  setupFilesAfterEnv: [
+    "<rootDir>/src/tests/setupTests.ts"
+  ],
+  transform: {
+      "^.+\\.(js|jsx|ts|tsx)": "<rootDir>/node_modules/babel-jest"
+  },
+    testEnvironment: 'jsdom',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "src/**/*.{tsx,ts}",
+        "!src/**/*.spec.tsx",
+    ],
+    coverageReporters: ["lcov", "json"]
+}
