22e20fb5069d9bf48fc33e37d1dee77d08a01f55
Eduardo Parducci <eduardo.parducci@dfki.de>
2021-08-09 16:24:19 +0200

    style: added default pages style.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -29,12 +29,13 @@ function Home() {
     }, []);
 
     return (
-        <>
-          <h1>Star Wars Movie List</h1>
-          {error  && <h2>An error occured, please refresh the page.</h2>}
-          {!error && loading && <h2>Fetching movies from the dark side...</h2>}
-          {!error && !loading && <CardsGrid movies={movies}/>}
-        </>
+    <>
+        <h1>Star Wars Movie List</h1>
+        <p>Click on the movie titles that you like the most to add them to your favorites list!</p>
+        {error  && <h2>An error occured, please refresh the page.</h2>}
+        {!error && loading && <h2>Fetching movies from the dark side...</h2>}
+        {!error && !loading && <CardsGrid movies={movies}/>}
+    </>
     );
 }
 
