a9228e0eda83f53edad68c63bc2a41968af917bf
Eduardo Parducci <eduardo.parducci@dfki.de>
2021-08-11 01:15:03 +0200

    refactor: changed favorites functionality.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../pages/home/Home';
-import Favorites from '../pages/favorites/Favorites';
 
 function Routes() {
 return (
@@ -9,10 +8,6 @@ return (
     <Route path="/" exact>
         <Home />
     </Route>
-    <Route path="/favorites" exact>
-        <Favorites />
-    </Route>
-    
 </Switch>
 );
 }
