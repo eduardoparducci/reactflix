679845ed63bb54f47b9a2b5696a032d30e3707e4
Eduardo Parducci <eduardo.parducci@dfki.de>
2021-08-12 00:40:53 -0300

    feat: created extra API methods

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,19 @@
 import axios from 'axios';
 
-const base_url = 'https://swapi.dev/api/';
+const base_url = 'https://swapi.dev/api';
 
 export async function getMovies() {
-    const response = await axios.get(`${base_url}films/`);
+    const response = await axios.get(`${base_url}/films/`);
     return response.data.results;
 }
+
+export async function getChar(url: string) {
+    const response = await axios.get(`${url}`);
+    return response.data;
+}
+
+export async function getMovieDetails(id: string) {
+    const response = await axios.get(`${base_url}/films/${id}`);
+    return response.data;
+}
+
